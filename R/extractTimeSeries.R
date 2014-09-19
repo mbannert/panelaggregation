72190d80bc30d695b84bc0dcc2495c790ce7328b
igbucur <ioan.gabriel.bucur@gmail.com>
2014-09-19 13:28:02 +0200

    added group_list == NULL branch to extractTimeSeries; added joinDataTables; updated documentation

--- R/extractTimeSeries.R
+++ R/extractTimeSeries.R
@@ -1,10 +1,10 @@
 #' Extract a Time Series from a Data.table 
 #' 
-#' This functions extracts time series from data.table columns and returns object of class ts. 
+#' This function extracts time series from data.table columns and returns object of class ts. 
 #' @author Matthias Bannert, Gabriel Bucur
 #' @param data_table a data.table 
 #' @param time_column character name of the column which contains the time index
-#' @param group_list list
+#' @param group_list list or NULL
 #' @param freq integer value either 4 denoting quarterly frequency or 12 denoting quarterly frequency
 #' @param item character name of the column which contains the item
 #' that is extracted from the data.table
@@ -20,7 +20,11 @@ extractTimeSeries <- function(data_table, time_column, group_list, freq,
   
   # Cross Join is used because J is not a function
   # and cannot be do.called
-  dt_subset <- data_table[do.call("CJ", group_list)]
+  if (is.null(group_list)) {
+    dt_subset <- data_table
+  } else {
+    dt_subset <- data_table[do.call("CJ", group_list)]
+  }
   
   start_date <- min(as.Date(dt_subset[[time_column]]))
   year <- as.numeric(format(start_date, "%Y"))
@@ -42,11 +46,17 @@ extractTimeSeries <- function(data_table, time_column, group_list, freq,
                frequency = freq)
   
   # generate key parts seperately to make key pasting easier to read
-  group_level <- paste(setdiff(names(group_list),
-                               time_column),collapse = "_")
-  ts_key <- paste(prefix, group_level,
-                  paste(unlist(group_list),collapse=":"),
-                  variable, item, sep = ".")
+  if (is.null(group_list)) {
+    group_level <- NULL
+    group_value <- NULL
+  } else {
+    group_level <- paste(setdiff(names(group_list),
+                                 time_column), collapse = ":")
+    group_value <- paste(unlist(group_list), collapse = ":")
+  }
+  
+  ts_key <- paste(c(prefix, group_level, group_value,
+                    variable, item), collapse = ".")
   
   attr(out_ts,"ts_key") <- toupper(ts_key)
   out_ts  
