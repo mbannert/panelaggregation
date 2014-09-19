40142b83eb7336d92d10df92393978907069368c
igbucur <ioan.gabriel.bucur@gmail.com>
2014-09-19 17:31:57 +0200

    Extended aggregation.R example with higher level aggregation

--- demo/aggregation.R
+++ demo/aggregation.R
@@ -9,28 +9,64 @@ levels(btsdemo$question_1) <- c("pos","eq","neg")
 
 # compute the weighted shares and display store in wide format 
 # to get a basis for further steps
-level1 <- computeShares(btsdemo,"question_1","weight",by = c("date_qtrly","group"))
+level1 <- computeShares(btsdemo,"question_1","weight",by = c("date_qtrly","group", "altGroup", "sClass"))
 
 # compute balance, don't have to do much here, because
-# 3 items is the default
+# (pos, eq, neg) is the default for the possible answers
 level1_wbalance <- computeBalance(level1)
 
 # Select a particular grouping combination and a timeseries that 
-# should be extracted from the data_table
+# should be extracted from the level 1 aggregation.
 ts1 <- extractTimeSeries(level1_wbalance,
-                  "date_qtrly",
-                  list(group = "C"),
-                  freq = 4,
-                  item = "balance",
-                  variable = "question_1")
+                         "date_qtrly",
+                         list(group = "C", altGroup = "a", sClass = "S"),
+                         freq = 4,
+                         item = "balance",
+                         variable = "question_1")
 ts1
 # Plot a standard R ts using the plot method for ts
-plot(ts1,main = attributes(ts1)$ts_key)
+plot(ts1, main = attributes(ts1)$ts_key)
 
-# DON'T RUN
-# Optionally you can store the series into a time series db using the
-# corresponding package... 
-# Note that you can use the attribute to specify a timeseries key dynamically ! 
-# library(timeseriesdb)
-# con <- dbConnect(PostgreSQL(),user = "yourname",dbname="yourdb",host = "yourhost")
-# storeTimeseries("ts1",attributes(ts1)$ts_key,"con")
+# Add weight column to the aggregated results
+# In order to join the tables, we need to know what weight to assign to each row.
+# This is done by having via a common key, for example c('group', 'altGroup').
+# In this example we would assign a different weight for each c('group', 'altGroup') combination (e.g. c('A', 'a')).
+btsweight1 <- btsdemo[, list(weight = sum(weight)), by = 'group']
+btsagg1 <- joinDataTables(level1_wbalance, btsweight1, 'group')
+
+# Compute second level aggregation, this time on fewer columns and using a different set of weights.
+level2_balance <- computeWeightedMean(btsagg1, 'balance', 'weight', c("date_qtrly","group", "sClass"))
+
+# Select a particular grouping combination and a timeseries that 
+# should be extracted from the level 2 aggregation.
+ts2 <- extractTimeSeries(level2_balance,
+                         "date_qtrly",
+                         list(group = "C", sClass = "S"),
+                         freq = 4,
+                         item = "balance",
+                         variable = "question_1")
+ts2
+# Plot a standard R ts using the plot method for ts
+plot(ts2, main = attributes(ts2)$ts_key)
+
+# Add weight column to the aggregated results
+# In order to join the tables, we need to know what weight to assign to each row.
+# This is done by having via a common key, for example c('group', 'altGroup').
+# In this example we would assign a different weight for each c('group', 'altGroup') combination (e.g. c('A', 'a')).
+btsweight2 <- btsdemo[, list(weight = sum(weight)), by = 'sClass']
+btsagg2 <- joinDataTables(level2_balance, btsweight2, 'sClass')
+
+# Compute third level of aggregation, on the whole sector, using yet another set of weights.
+level3_balance <- computeWeightedMean(btsagg2, 'balance', 'weight', c("date_qtrly", "sClass"))
+
+# Select a particular grouping combination and a timeseries that 
+# should be extracted from the level 2 aggregation.
+ts3 <- extractTimeSeries(level3_balance,
+                         "date_qtrly",
+                         list(sClass = "S"),
+                         freq = 4,
+                         item = "balance",
+                         variable = "question_1")
+ts3
+# Plot a standard R ts using the plot method for ts
+plot(ts3, main = attributes(ts3)$ts_key)
