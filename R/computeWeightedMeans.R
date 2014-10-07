051abd53f61e8d28302079d829f4616bc3f11cf9
igbucur <ioan.gabriel.bucur@gmail.com>
2014-10-07 10:32:09 +0200

    upgraded ComputeWeightedMean to ComputeWeightedMeans - faster and supports multiple variables

--- R/computeWeightedMeans.R
+++ R/computeWeightedMeans.R
@@ -6,22 +6,20 @@
 #' 
 #' @author Matthias Bannert, Gabriel Bucur
 #' @param data_table a data.table
-#' @param variable character name of the variable to focus on. The variable must be in the data.table
+#' @param variables character name of the variable(s) to focus on. The variables must be in the data.table
 #' @param weight character name of the data.table column that contains a weight. 
 #' @param by character vector of the columns to group by
 #' @import data.table
 #' @example demo/aggregation.R
 #' @export
-computeWeightedMean <- function(data_table, variable, weight, by) {
+computeWeightedMeans <- function(data_table, variables, weight, by) {
   
   if (is.null(weight)) {
-    res_dt <- data_table[, list(wavg = mean(eval(as.name(variable)), na.rm = TRUE)), by = by]
+    res_dt <- data_table[, lapply(.SD, mean, na.rm = TRUE), .SDcols = variables, by = by]
   } else {
-    res_dt <- data_table[, list(wavg = weighted.mean(eval(as.name(variable)), 
-                                                     eval(as.name(weight)), na.rm = TRUE)), 
-                         by = by]
+    res_dt <- data_table[, lapply(.SD, weighted.mean, weight = eval(as.name(weight)), na.rm = TRUE),
+                         .SDcols = variables, by = by]
   }
   
-  setnames(res_dt, 'wavg',variable)
   res_dt
 }
